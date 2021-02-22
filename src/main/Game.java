a3bda61107153e0c56c3831b9f2359e855732968
alexbuntaran <alexbuntaran@kingsstudent.org>
2021-02-22 15:33:14 -0800

    added engine

--- src/main/Game.java
+++ src/main/Game.java
@@ -1,13 +1,50 @@
 package main;
 
-public class Game {
+public class Game implements Runnable {
+
+    private Thread thread;
+    private boolean running;
 
     public Game() {
+        
+    }
+
+    private void init() {
 
     }
 
-    public void start() {
-        
+    private void update() {
+
+    }
+
+    private void render() {
+
+    }
+
+    @Override
+    public void run() {
+        init();
+        while (running) {
+            update();
+            render();
+        }
+
+        stop();
+    }
+
+    public synchronized void start() {
+        running = true;
+        thread = new Thread(this);
+        thread.start();
+    }
+
+    public synchronized void stop() {
+        running = false;
+        try {
+            thread.join();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
     
 }
